c997bb8dc2f70b1483f5d716812e60b781041999
sahil <sahilghangash950@gmail.com>
2022-09-26 16:06:57 +0530

    make it better

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: process.env.REACT_APP_API_ENDPOINT });
+const API = axios.create({
+  baseURL: "https://mern-clone-instagram.herokuapp.com",
+});
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("userData")) {
